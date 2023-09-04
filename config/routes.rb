91a1ae45f9550be5a7317ee7a4cf5f4ea91b0650
Guille Zorron <gzorron@xmartlabs.com>
2023-09-04 12:49:05 -0300

    pundit index working

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
   resources :cancancan_posts, only: :index
+  resources :pundit_posts, only: %i[index]
 end
